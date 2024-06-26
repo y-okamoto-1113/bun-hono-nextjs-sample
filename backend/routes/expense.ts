ddb0c5fe2b06c005fa9f8be763700472eb211d0e
y-okamoto-1113 <okamoto.yuuki.1113+github@gmail.com>
2024-06-27 02:58:59 +0900

    :recycle: Refactored the definition of type Expense

--- backend/routes/expense.ts
+++ backend/routes/expense.ts
@@ -1,33 +1,52 @@
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { z } from "zod";
 
-type Expense = {
-  id: number;
-  title: string;
-  amount: number;
-};
+const expenseScehema = z.object({
+  id: z.number().int().positive().min(1),
+  title: z.string().min(3).max(100),
+  amount: z.number().int().positive(),
+});
 
+type Expense = z.infer<typeof expenseScehema>;
 const fakeExpenses: Array<Expense> = [
   { id: 1, title: "食費", amount: 1000 },
   { id: 2, title: "交通費", amount: 2000 },
   { id: 3, title: "住宅費", amount: 3000 },
 ];
 
-const ccreateExpenseSchema = z.object({
-  title: z.string().min(3).max(100),
-  amount: z.number().int().positive(),
-});
+const createExpenseSchema = expenseScehema.omit({ id: true });
 
 export const expenseRoute = new Hono()
   .get("/", async (c) => {
     return c.json({ expenses: fakeExpenses });
   })
-  .post("/", async (c) => {
+  .post("/", zValidator("json", createExpenseSchema), async (c) => {
     const data = await c.req.json();
-    const expense = ccreateExpenseSchema.parse(data);
+    const expense = createExpenseSchema.parse(data);
+    fakeExpenses.push({ ...expense, id: fakeExpenses.length + 1 });
+    c.status(201);
     return c.json({ expense });
+  })
+  .get("/:id{[0-9]+}", async (c) => {
+    const id = Number.parseInt(c.req.param("id"));
+    const expense = fakeExpenses.find((expense) => expense.id === id);
+    if (!expense) {
+      c.notFound();
+    } else {
+      return c.json({ expense });
+    }
+  })
+  .delete("/:id{[0-9]+}", async (c) => {
+    const id = Number.parseInt(c.req.param("id"));
+    const index = fakeExpenses.findIndex((expense) => expense.id === id);
+    if (index === -1) {
+      c.notFound();
+    } else {
+      const deletedExpense = fakeExpenses.splice(index, 1)[0];
+      return c.json({ expense: deletedExpense });
+    }
   });
-// .delete
 // .put
 
 export default expenseRoute;
